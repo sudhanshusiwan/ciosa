904f87e6eabfe8baa20fb5b0ea350849be896bd4
Kanmani <kanmaniselvan.m@in.reportbee.com>
2016-11-13 03:03:52 +0530

    Added a helper method to get user cart product count.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :cart_product_count
+
+  def cart_product_count
+    if current_user.present?
+      current_user.cart_products.size
+    else
+      0
+    end
+  end
 end
