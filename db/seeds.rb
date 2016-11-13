f184eeaa4f3b6b26ee629aeab38a0ebc661ee2bb
Sudhanshu <sudhanshusiwan@gmail.com>
2016-11-13 11:48:30 +0530

    removing code from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-Spree::Core::Engine.load_seed if defined?(Spree::Core)
-Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
