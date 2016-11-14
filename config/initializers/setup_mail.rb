ce5f9c1da9f177ff3b8d1b8f7734600401e10eaf
Kanmani <kanmaniselvan.m@in.reportbee.com>
2016-11-14 22:34:27 +0530

    Removing login details

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :user_name            => "kanmaniselvan",
-    :password             => "AAaa11!!",
-    :domain               => "reportbee.com",
-    :address              => "smtp.sendgrid.net",
-    :port                 => 587,
-    :authentication       => :plain,
-    :enable_starttls_auto => true
-}
